1f017857541031d29ca77661695e11555a1866ff
mutesialine <ktesialine@gmail.com>
2025-06-23 18:43:03 +0200

    chore: add AWS S3 upload provider dependency to package.json

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,37 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "strapistorage.blr1.amazonaws.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "https://strapistorage.s3.blr1.amazonaws.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,21 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: env("UPLOAD_PROVIDER"),
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("DO_SPACE_REGION",),
+        endpoint: env("DO_SPACE_ENDPOINT"),
+        params: {
+          Bucket: env("AWS_BUCKET"),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
